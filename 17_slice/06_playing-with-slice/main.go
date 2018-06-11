ad60616a03b8542b3da2182883711b2c5ed612a2
Prateek Sharma <meprateek1986@gmail.com>
2018-06-11 22:30:47 +0530

    slice nil check

--- 17_slice/06_playing-with-slice/main.go
+++ 17_slice/06_playing-with-slice/main.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	aSlice := []string{}               //empty string slice
+	var nilSlice []string              //only way to create a nil slice
 	var newSlice = make([]string, 5)   //making a string slice with length 5. This will by default choose capacity 5.
 	var nSlice = make([]string, 5, 15) //making a slice of length 5 and capacity 15
 
+	fmt.Println(nilSlice == nil)
 	fmt.Println(aSlice == nil)
 	fmt.Println(cap(newSlice))
 	fmt.Println(cap(nSlice))
